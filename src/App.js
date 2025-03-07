e6ef7d9e02ac4df8be4e7ea9dbfa4aa2f0d5ce7a
Sachin Sharma <ss0382245@gmail.com>
2025-03-07 16:37:12 +0530

    basic class component app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UserFinder from './components/UserFinder';
+import "./components/UserFinder.module.css"
+
+function App() {
+  return (
+    <div>
+      <UserFinder />
+    </div>
+  );
+}
+
+export default App;
